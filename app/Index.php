aef858b672578eefd42a652773b428551e468b19
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-03-10 20:11:23 +0530

    Changed Files

--- app/Index.php
+++ app/Index.php
@@ -208,9 +208,10 @@ return $ExtrafeeConfig;
 
     public function test123456() {
         echo "test 111";
-    } public function test123456() {
-    echo "test 111";
-}
+    }
+    public function test123456() {
+        echo "test 111";
+    }
 
     public function test123456() {
         echo "test 111";
