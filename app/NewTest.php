0e16827c1960e904a0fd9fe22af25805dd19d527
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-03-21 15:56:39 +0530

    Changed Files

--- app/NewTest.php
+++ app/NewTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Magecomp\Extrafee\Model;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
@@ -25,215 +26,96 @@ class NewTest implements ConfigProviderInterface
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Psr\Log\LoggerInterface $logger
      */
-public function __construct(
-\Magecomp\Extrafee\Helper\Data $dataHelper,
-\Magento\Checkout\Model\Session $checkoutSession,
-    \Psr\Log\LoggerInterface $logger
+    public function __construct(
+        \Magecomp\Extrafee\Helper\Data  $dataHelper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Psr\Log\LoggerInterface        $logger
 
-)
+    )
     {
-$this->dataHelper = $dataHelper;
- $this->checkoutSession = $checkoutSession;
- $this->logger = $logger;
-        
+        $this->dataHelper = $dataHelper;
+        $this->checkoutSession = $checkoutSession;
+        $this->logger = $logger;
+
     }
 
     /**
      * @return array
      */
     //There is new rgjknrfk
-public function getConfig()
-{
-    echo "11";
+    public function getConfig()
+    {
 
-    echo "11 222 33";
-
-    echo "111";
-    echo "sdasda sd";
-
-    echo "dsd s ";
-
-
-    echo "dsd s ";
-    echo "sdasda sd new";
-
-    echo "dsd s ";    echo "dsd s ";
-
-    echo "sdasda sd new";   echo "sdasda sd new";
-    echo "sdasda sd new"; echo "sdasda sd new"; echo "sdasda sd new";
-    echo "sdasdad";
-.
-    echo "dsd s ";
-    echo "dsd s ";
-
-    .
-    echo "dsd s ";
-    echo "dsd s ";
-
-    .
-    echo "dsd s ";
-    echo "dsd s ";
-    .
-    echo "dsd s ";
-    echo "dsd s ";
-
-
-    falsef
-    foreach ( as $item) {
-
-        
-    }
-      echo "sdasda sd new";
- echo "dsd s ";
-          echo "dsd s ";
-    echo "dsd s ";
-    echo "dsdsd";
-    echo "dsdsd";
-    echo "fff";
-    echo "dsdsd";
-    echo "fff";
-    echo "dsdsd";
-    echo "fff";
-    echo "dsdsd";
-    echo "fff";
-
-    echo "dsdsd";
-    echo "fff";
-    echo "dsd s ";
-    echo "dsd s ";
-    echo "dsdsd";
-    echo "dsdsd";
-    echo "sdsd sadsadas sdsd";
-    echo "sd sdasda dsd";
-    echo "fff sdsd sd";
-
-
-    echo "dsdsd";
-    echo "fff";
-
-    echo "dsdsd";
-    echo "fff";
-
-    echo "dsdsd";
-    echo "fff";    echo "dsdsd";
-    echo "fff";
-
-    echo "dsdsd";
-
-    echo "dsds";
-    echo "sdd";
-
-	
 //This is for testing sdjvn
-$ExtrafeeConfig = [];
- $enabled = $this->dataHelper->isModuleEnabled();
- $minimumOrderAmount = $this->dataHelper->getMinimumOrderAmount();
-$ExtrafeeConfig['fee_label'] = $this->dataHelper->getFeeLabel();
-$quote = $this->checkoutSession->getQuote();
-$subtotal = $quote->getSubtotal();
-$ExtrafeeConfig['custom_fee_amount'] = $this->dataHelper->getExtrafee();
-$ExtrafeeConfig['show_hide_Extrafee_block'] = ($enabled && ($minimumOrderAmount <= $subtotal) && $quote->getFee()) ? true : false;
-$ExtrafeeConfig['show_hide_Extrafee_shipblock'] = ($enabled && ($minimumOrderAmount <= $subtotal)) ? true : false;
-return $ExtrafeeConfig;
+        $ExtrafeeConfig = [];
+        $enabled = $this->dataHelper->isModuleEnabled();
+        $minimumOrderAmount = $this->dataHelper->getMinimumOrderAmount();
+        $ExtrafeeConfig['fee_label'] = $this->dataHelper->getFeeLabel();
+        $quote = $this->checkoutSession->getQuote();
+        $subtotal = $quote->getSubtotal();
+        $ExtrafeeConfig['custom_fee_amount'] = $this->dataHelper->getExtrafee();
+        $ExtrafeeConfig['show_hide_Extrafee_block'] = ($enabled && ($minimumOrderAmount <= $subtotal) && $quote->getFee()) ? true : false;
+        $ExtrafeeConfig['show_hide_Extrafee_shipblock'] = ($enabled && ($minimumOrderAmount <= $subtotal)) ? true : false;
+        return $ExtrafeeConfig;
     }
 
-    public function testData() {
+    public function testData()
+    {
         echo "Dsdsd"
-        echo "sdsdd   "  ;
+        echo "sdsdd   ";
     }
 
-    public function testData2() {
-        echo "Dsdsd"
-        echo "sdsdd   "
-   echo "Dsdsd"
-        echo "sdsdd   "
-        sadsdds
-        ;
-    }
-    public function testData2() {
+    public function testData2()
+    {
         echo "Dsdsd"
         echo "sdsdd   "
    echo "Dsdsd"
         echo "sdsdd   "
-        sadsdds
-        ;
+        sadsdds;
     }
 
-    public function testData2() {
-        echo "Dsdsd"
-        echo "sdsdd   "
-   echo "Dsdsd"
-        echo "sdsdd   "
-        sadsdds
-        ;
-    }
 
-    public function test() {
-        echo "test 111 ddd";
-    }
 
-    public function test123() {
+    public function test123()
+    {
         echo "test 111";
     }
 
-    public function test123456() {
+    public function test123456()
+    {
         echo "test 111";
     }
 
-    public function test123456() {
-        echo "test 111";
-    }
 
-    public function test123456() {
-        echo "test 111";
-    }
-    public function test123456() {
-        echo "test 111";
-    }
-    public function test123456() {
-        echo "test 111";
-    }
+public function pqr()
+{
+    echo "test 111 222";
+}
 
-    public function test123456() {
-        echo "test 111";
-    }
-
-    public function 1111pqr() {
-        echo "test 111 222";
-    }
-
-    public function pqr() {
-        echo "test 111 222";
-    }
-    public function test123456() {
-        echo "test 111";
-    }
-
-public function test123456() {
-    echo "test 111";
-}    public function test123456() {
+public
+function test123456()
+{
     echo "test 111";
 }
 
-public function test123456() {
+public
+function test123456()
+{
     echo "test 111";
 }
 
-public function test123456() {
+public
+function test123456()
+{
     echo "test 111";
 }
 
-public function test123456() {
-    echo "test 111";
-}
-public function test123456() {
-    echo "test 111";
-}
-public function test123456() {
+public
+function test123456()
+{
     echo "test 111";
 }
 
-public function test1234563333() {
-    echo "test 111";
-}
+
+
 }
