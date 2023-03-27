175a5a20b2aaa12ff2b06f2076406647a66f00fd
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-03-27 18:58:48 +0530

    Fix codesniffer

--- app/Index.php
+++ app/Index.php
@@ -1,13 +1,4 @@
 <?php
-
-    function test() {
-        test 222;
-    }
-
-function test44() {
-    test 222;
-    }
-
 function test4455() {
     test 222;
     }
@@ -27,11 +18,11 @@ function test44535() {
 }
 function test44535() {
     echo "test 222 aasas";
-    cho "dsdsd";
+    echo "dsdsd";
     echo "sdasda ";
 }
 
-function test() {{
+function test() {
 
-}}
+}
 ?>
